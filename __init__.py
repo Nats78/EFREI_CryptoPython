883dad8f81369eed1f994109cb0ad9aae86c9473
Nats78 <nathan.blr@orange.fr>
2025-01-14 11:43:02 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('Exemple_Base_SVG.html')
+    return render_template('Valet_de_Trèfle.SVG')
 
 # Génération de la clé pour le chiffrement/déchiffrement
 key = Fernet.generate_key()
